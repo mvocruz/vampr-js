16e05d6f5adb40fe0ed03f5b4f2b1d6292cf3f1e
Marcio Cruz <marciocruz@Marcios-MacBook-Pro.local>
2020-11-13 20:46:51 -0800

    changes

--- vampire.js
+++ vampire.js
@@ -10,22 +10,40 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
+    return this.offspring.length;
 
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numVamp = 0;
+    let currentVamp = this;
+
+    while (currentVamp.creator) {
+      currentVamp = currentVamp.creator;
+      numVamp++;
+    }
+    return numVamp
 
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
+    let vampSenior = vampire.numberOfVampiresFromOriginal;
+    let mySenior = this.numberOfVampiresFromOriginal;
 
+    if (vampSenior > mySenior) {
+      return true
+    } 
+    else {
+      return false
+    }
   }
 
   /** Stretch **/
